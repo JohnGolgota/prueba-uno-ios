61d57526204f1fe82cc0f66e592cab0ea52eaab9
JohnGolgota <104475729+JohnGolgota@users.noreply.github.com>
2024-05-29 17:02:25 -0500

    initial commit

---
+++ pruebauno/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pruebauno
+//
+//  Created by usuario on 29/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pruebauno/pruebaunoApp.swift
@@ -0,0 +1,17 @@
+//
+//  pruebaunoApp.swift
+//  pruebauno
+//
+//  Created by usuario on 29/05/24.
+//
+
+import SwiftUI
+
+@main
+struct pruebaunoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
